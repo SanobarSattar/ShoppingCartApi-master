36409088c9dbbb5214a675425a395b4319a943bc
SanobarSattar <30503019+SanobarSattar@users.noreply.github.com>
2017-09-15 12:38:54 +0500

    Initial commit

---
+++ ShoppingCartApi/Controllers/ShoppingCartController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace ShoppingCartApi.Controllers
+{
+
+    public class ShoppingCartController : ApiController
+    {
+        public class MyCart
+        {
+
+            public List<Item> Items { get; set; }
+
+
+        }
+        public class Item
+        {
+            public int Count { get; set; }
+
+            public Product Product { get; set; }
+
+        }
+        
+        
+        MyShoppingCartEntities db = new MyShoppingCartEntities();
+      
+        [Route("api/GetProducts")]
+        [HttpGet]
+        public List<Product> GetProducts()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+
+            return db.Products.ToList();
+        }
+        [HttpGet]
+        [Route("api/AddToCart/{Id}")]
+        public void AddToCart(int Id)
+        {
+            MyCart sc = null;
+
+            if (HttpContext.Current.Session["sc"] == null)
+                sc = new MyCart();
+            else
+                sc = HttpContext.Current.Session["sc"] as MyCart;
+
+            if (sc.Items == null)
+                sc.Items = new List<Item>();
+
+            var db = new MyShoppingCartEntities();
+            db.Configuration.ProxyCreationEnabled = false;
+
+            var prod = db.Products.Find(Id);
+
+            Item item = new Item();
+            item.Count = 1;
+            item.Product = prod;
+
+            sc.Items.Add(item);
+
+            HttpContext.Current.Session["sc"] = sc;
+        }
+
+        [HttpGet]
+        [Route("api/GetCart")]
+       
+        public MyCart GetCart()
+        {
+            
+             return HttpContext.Current.Session["sc"] as MyCart;
+            
+        }
+    }
+}
